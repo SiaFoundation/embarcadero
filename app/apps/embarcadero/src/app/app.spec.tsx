f45b4915e585d537013a431285486f081efe4bc6
Alex Freska <alex.freska@gmail.com>
2022-02-16 17:51:07 -0500

    feat: transplant app

---
+++ app/apps/embarcadero/src/app/app.spec.tsx
@@ -0,0 +1,15 @@
+describe('App', () => {
+  it('true', () => {
+    expect(true).toBeTruthy()
+  })
+
+  // it('should have a greeting as the title', () => {
+  //   const { getAllByText } = render(
+  //     <BrowserRouter>
+  //       <App />
+  //     </BrowserRouter>
+  //   )
+
+  //   expect(getAllByText(/Embarcadero/gi)).toBeTruthy()
+  // })
+})
