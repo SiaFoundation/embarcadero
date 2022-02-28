5bd9b30e24509fef5b8efa7dc1e1c6565297eb58
Alex Freska <alex.freska@gmail.com>
2022-02-28 13:06:35 -0500

    feat: sequencing of steps and error states

--- app/apps/embarcadero/src/app/hooks/useConsensus.tsx
+++ app/apps/embarcadero/src/app/hooks/useConsensus.tsx
@@ -1,38 +1,14 @@
 import useSWR from 'swr'
 import { getApi } from '../../config'
 import { ConsensusGET } from '@siafoundation/sia-js'
-
-interface SWRError extends Error {
-  info?: string
-  status?: number
-}
+import { handleResponse } from './handleResponse'
 
 export function useConsensus() {
   return useSWR<ConsensusGET>(
     'consensus',
     async () => {
-      let res: Response | undefined = undefined
-      // try {
-      res = await fetch(getApi('/api/consensus'))
-      // } catch (e) {
-      //   if (e instanceof Error) {
-      //     throw e
-      //   }
-      // }
-
-      // If the status code is not in the range 200-299,
-      // we still try to parse and throw it.
-      if (!res || !res.ok) {
-        const message = await res?.text()
-        const error: SWRError = new Error(
-          message || 'An error occurred while fetching the data.'
-        )
-        // Attach extra info to the error object.
-        error.status = res?.status || 500
-        throw error
-      } else {
-        return res.json()
-      }
+      const response = await fetch(getApi('/api/consensus'))
+      return handleResponse(response)
     },
     {
       refreshInterval: 10_000,
